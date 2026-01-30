25212360a6c3d258dce2224c57c0f30d71e75662
Sergio Balerdi <pegasus.prodescargas@gmail.com>
2026-01-29 21:12:51 -0300

    agrego refresh token en los tokens

--- src/pegasus_framework/auth/dto/token_pair.py
+++ src/pegasus_framework/auth/dto/token_pair.py
@@ -6,4 +6,6 @@ from datetime import datetime
 class TokenPairDTO:
     access_token: str
     expires_at: datetime
+    refresh_token: str
+    refresh_token_expires_at: datetime
     token_type: str = "bearer"
