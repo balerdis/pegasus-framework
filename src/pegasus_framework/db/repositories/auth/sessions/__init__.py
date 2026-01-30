f521ac7e68ad1a06bee53500681b55bcb74e9de3
Sergio Balerdi <pegasus.prodescargas@gmail.com>
2026-01-30 15:52:51 -0300

    mejoro nomenclatura de las clases

--- src/pegasus_framework/db/repositories/auth/sessions/__init__.py
+++ src/pegasus_framework/db/repositories/auth/sessions/__init__.py
@@ -1,5 +1,9 @@
 from .session_repository import SessionRepository
+from .auth_session_repository_base import AuthSessionRepositoryBase
+from .auth_session_token_repository_base import AuthSessionTokenRepositoryBase
 
 __all__ = [
     "SessionRepository",
+    "AuthSessionRepositoryBase",
+    "AuthSessionTokenRepositoryBase"
 ]
