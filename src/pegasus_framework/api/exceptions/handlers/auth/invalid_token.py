656ac14c232f34faf7f6fcd243761034867edefa
Sergio Balerdi <pegasus.prodescargas@gmail.com>
2026-02-03 21:59:01 -0300

    comienzo a tener en cuenta los mensajes de las excepciones que llegan al handler registrado

--- src/pegasus_framework/api/exceptions/handlers/auth/invalid_token.py
+++ src/pegasus_framework/api/exceptions/handlers/auth/invalid_token.py
@@ -1,3 +1,4 @@
+# pegasus_framework/api/exceptions/handlers/auth/invalid_token.py
 from fastapi import Request, status
 from fastapi.responses import JSONResponse
 from pegasus_framework.api.v1.schemas.generic import ErrorResponse
@@ -8,9 +9,11 @@ async def invalid_token_handler(
     request: Request,
     exc: InvalidTokenError,
 ):
+    message = str(exc) if str(exc) else "Invalid or expired token"
+
     payload = ErrorResponse(
         success=False,
-        message="Invalid or expired token",
+        message=message,
         error_code="INVALID_TOKEN",
         details=None,
     ).model_dump()
